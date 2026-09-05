e065303a1557bbf0ce05832028ebcc0c0a772022
Ahmed Hillawi <hillawi.ahmed@gmail.com>
2026-09-05 19:43:55 +0200

    Added inline stepper to increase/decrease item quantity
    # Conflicts:
    #	app/src/main/java/com/ahmedhillawi/myshoppinglist/domain/MeasurementUnit.kt

--- app/src/main/java/com/ahmedhillawi/myshoppinglist/domain/MeasurementUnit.kt
+++ app/src/main/java/com/ahmedhillawi/myshoppinglist/domain/MeasurementUnit.kt
@@ -6,13 +6,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class MeasurementUnit(@StringRes val resId: Int) {
-    PACK(R.string.unit_pack),
-    ML(R.string.unit_ml),
+enum class MeasurementUnit(@StringRes val resId: Int, val step: Double) {
+    PACK(R.string.unit_pack, 1.0),
+    ML(R.string.unit_ml, 50.0),
     @SerialName("L")
-    LITRE(R.string.unit_l),
+    LITRE(R.string.unit_l, 0.5),
     @SerialName("G")
-    GRAM(R.string.unit_g),
-    KG(R.string.unit_kg),
-    PCS(R.string.unit_pcs)
+    GRAM(R.string.unit_g, 50.0),
+    KG(R.string.unit_kg, 0.5),
+    PCS(R.string.unit_pcs, 1.0)
 }
